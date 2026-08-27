74dd41f6f388449135ccb89616624c8abb91734c
KathM00 <kathymontamejia@gmail.com>
2026-08-27 10:25:15 -0400

    feat: devolver tambien frizzbuzz si es multiplo de 3 Y 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,13 +1,13 @@
 function fizzbuzz(num) {
-  if (num % 3 === 0) {
+ if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  }  
+if (num % 3 === 0) {
     return "Fizz";
   }
-  if (num % 5 === 0) {
+if (num % 5 === 0) {
     return "Buzz";
   }
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } 
   return num + "";
 }
 
